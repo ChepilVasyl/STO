7bf5149c4587dcf89ac4a91245ad0ce41055dfa1
ChepilVasyl <lalala19la95@gmail.com>
2025-02-13 19:19:35 +0200

    Initial commit

---
+++ src/app/components/allNotification.tsx
@@ -0,0 +1,9 @@
+import {useEffect, useState} from "react";
+import {AwaiterMulti} from "next/dist/server/after/awaiter";
+
+export default () => {
+    const [notifications, setNotifications] = useState([]);
+    useEffect(() => {
+        const apiRequest = fetch('')
+    })
+}
